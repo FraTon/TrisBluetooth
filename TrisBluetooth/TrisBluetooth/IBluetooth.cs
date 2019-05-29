998dc407517514ea68ff46d619b798abc7a68624
FraTon <francio.97@hotmail.it>
2019-05-29 17:56:50 +0200

    1

---
+++ TrisBluetooth/TrisBluetooth/IBluetooth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TrisBluetooth
+{
+    public interface IBluetooth
+    {
+        void Start(string name, int sleepTime, bool readAsCharArray);
+        void Cancel();
+        ObservableCollection<string> PairedDevices();
+      
+    }
+}
--- TrisBluetooth/TrisBluetooth/MainPage.xaml.cs
+++ TrisBluetooth/TrisBluetooth/MainPage.xaml.cs
@@ -11,7 +11,35 @@ namespace TrisBluetooth
     {
         public MainPage()
         {
-            InitializeComponent();
+            this.BindingContext = new ViewModel();
+
+            Picker cerca = new Picker  { Title = "Cerca" };
+            cerca.SetBinding(Picker.ItemsSourceProperty, "ListOfDevices");
+            cerca.SetBinding(Picker.SelectedItemProperty, "SelectedBthDevice");
+            cerca.SetBinding(VisualElement.IsEnabledProperty, "IsPickerEnabled");
+
+            Button buttonConnect = new Button() { Text = "Connect" };
+            buttonConnect.SetBinding(Button.CommandProperty, "ConnectCommand");
+            buttonConnect.SetBinding(VisualElement.IsEnabledProperty, "IsConnectEnabled");
+
+            Button buttonDisconnect = new Button() { Text = "Disconnect" };
+            buttonDisconnect.SetBinding(Button.CommandProperty, "DisconnectCommand");
+            buttonDisconnect.SetBinding(VisualElement.IsEnabledProperty, "IsDisconnectEnabled");
+
+            StackLayout slButtons = new StackLayout() { Orientation = StackOrientation.Horizontal, Children = { buttonDisconnect, buttonConnect } };
+
+            
+
+            int topPadding = 0;
+            if (Device.RuntimePlatform == Device.iOS)
+                topPadding = 20;
+
+            StackLayout sl = new StackLayout { Children = { cerca, slButtons, lv }, Padding = new Thickness(0, topPadding, 0, 0) };
+            Content = sl;
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
         }
     }
 }
